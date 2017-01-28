2231f38e7ea599160aecc16ba84ce690020438e3
Paul Crane <paul@acheronsoft.com>
2017-01-28 01:17:53 -0600

    Add basic structure for Path and PathRange types.

--- source/dpathutils/path.d
+++ source/dpathutils/path.d
@@ -1,1 +1,95 @@
 module dpathutils.path;
+
+import std.path;
+import std.algorithm.searching : all;
+import std.string : replace;
+import std.traits;
+import std.array;
+
+import std.stdio : writeln;
+
+private string buildNormalizedPathEx(const string[] paths...)
+{
+	if(all!`a is null`(paths))
+	{
+		return null;
+	}
+
+	if(all!`a == ""`(paths))
+	{
+		return "";
+	}
+
+	auto result = buildNormalizedPath(paths);
+
+	version(Posix)
+	{
+		result = result.replace(`\`, `/`);
+	}
+
+	return result == "" ? "." : result;
+}
+
+struct PathRange
+{
+public:
+	this(const string path)
+	{
+		range_ = pathSplitter(path.buildNormalizedPath);
+	}
+
+	string asString()
+	{
+		return range_.join("/").buildNormalizedPath;
+	}
+
+private:
+	auto getPathSplitterReturnType()
+	{
+		return pathSplitter(".");
+	}
+
+	//INFO: Since phobos makes use of voldemort types we have to work to get the actual type that pathSplitter returns.
+	ReturnType!(PathRange.getPathSplitterReturnType) range_;
+
+	alias range_ this;
+}
+
+struct Path
+{
+public:
+
+	this(const string path)
+	{
+		path_ = buildNormalizedPathEx(path);
+	}
+
+	string asString()
+	{
+		return path_;
+	}
+
+private:
+	string path_ = ".";
+}
+
+unittest
+{
+	auto path = PathRange("/home/zekereth/stuff");
+
+	writeln(buildNormalizedPathEx("\\home/soulsbane"));
+	writeln(path.back);
+	writeln(path.front);
+	writeln(path.asString);
+
+	writeln("------------------");
+	foreach(dir; path)
+	{
+		writeln(dir);
+	}
+
+	assert(buildNormalizedPathEx() == "");
+	assert(buildNormalizedPathEx(null) == null);
+	assert(buildNormalizedPathEx(null) == "");
+}
+
